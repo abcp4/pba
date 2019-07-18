45786ea0f807751e54f777aa6beb416d37e66a5f
Ivan Pereira <navi1921@gmail.com>
2019-07-18 16:02:37 -0300

    Update test_search.sh

--- scripts/test_search.sh
+++ scripts/test_search.sh
@@ -5,10 +5,10 @@ python pba/search.py \
     --local_dir "$PWD/results/" \
     --model_name wrn_40_2 \
     --dataset test \
-    --train_size 50 --val_size 50 \
+    --train_size 1000 --val_size 1322 \
     --checkpoint_freq 0 \
-    --name "test_search" --gpu 1 --cpu 2 \
-    --num_samples 4 --perturbation_interval 3 --epochs 160 \
+    --name "test_search" --gpu 0.19 --cpu 2 \
+    --num_samples 16 --perturbation_interval 3 --epochs 160 \
     --explore cifar10 --aug_policy cifar10 \
-    --lr 0.1 --wd 0.0005 --bs 16 --test_bs 16 --recompute_dset_stats
+    --lr 0.1 --wd 0.005 --bs 16 --test_bs 16 --recompute_dset_stats
 
