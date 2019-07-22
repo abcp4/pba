055e2814012c50f318afac638ee804531cc8c4ec
Ivan Pereira <navi1921@gmail.com>
2019-07-22 17:35:44 -0300

    Update test_search.sh

--- scripts/test_search.sh
+++ scripts/test_search.sh
@@ -5,6 +5,7 @@ python pba/search.py \
     --local_dir "$PWD/results/" \
     --model_name wrn_40_2 \
     --dataset test \
+    --data_path /content/data/
     --train_size 1000 --val_size 1322 \
     --checkpoint_freq 0 \
     --name "test_search" --gpu 0.19 --cpu 2 \
