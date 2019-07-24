fa8fb68fdec4bcf0c7d1a0d9dc13bfd05ee3fdc2
Ivan Pereira <navi1921@gmail.com>
2019-07-24 16:38:59 -0300

    Update samantha.sh

--- scripts/samantha.sh
+++ scripts/samantha.sh
@@ -11,5 +11,5 @@ python pba/search.py \
     --name "test_search" --gpu 0.15 --cpu 2 \
     --num_samples 16 --perturbation_interval 3 --epochs 200 \
     --explore cifar10 --aug_policy cifar10 \
-    --lr 0.1 --wd 0.0005 
+    --lr 0.1 --wd 0.0005 --bs 8 --test_bs 8 --recompute_dset_stats
 
