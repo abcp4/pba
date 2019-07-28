2b35f2114c525573ab0e615f8f35ec945e0e8d7d
Ivan Pereira <navi1921@gmail.com>
2019-07-28 14:42:36 -0300

    Update colab.sh

--- scripts/colab.sh
+++ scripts/colab.sh
@@ -8,7 +8,6 @@ python pba/search.py \
     --data_path /content/data/ \
     --train_size 1000 --val_size 1322 \
     --checkpoint_freq 5 \
-    --restore /content/checkpoint/ \
     --name "test_search" --gpu 0.15 --cpu 2 \
     --num_samples 16 --perturbation_interval 3 --epochs 200 \
     --explore cifar10 --aug_policy cifar10 \
