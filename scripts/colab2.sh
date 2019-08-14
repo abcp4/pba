0a79d05906b30f7d7ce2210415cfab26722c7a34
Ivan Pereira <navi1921@gmail.com>
2019-08-14 20:47:34 -0300

    Update colab2.sh

--- scripts/colab2.sh
+++ scripts/colab2.sh
@@ -3,7 +3,7 @@ export PYTHONPATH="$(pwd)"
 
 python pba/search2.py \
     --local_dir "$PWD/results/" \
-    --model_name pyramid_net \
+    --model_name shake_shake_32 \
     --dataset test \
     --data_path /content/data/ \
     --train_size 2600 --val_size 2600 \
