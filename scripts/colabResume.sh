121f2a9a2014311aca76c9b4f6f49abce5684438
Ivan Pereira <navi1921@gmail.com>
2019-08-15 16:53:51 -0300

    Create colabResume.sh

---
+++ scripts/colabResume.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export PYTHONPATH="$(pwd)"
+
+python pba/search2.py \
+    --local_dir "$PWD/results/" \
+    --model_name wrn_40_2 \
+    --dataset test \
+    --data_path /content/data/ \
+    --train_size 2600 --val_size 2600 \
+    --checkpoint_freq 5 \
+    --name "test_search" --gpu 0.15 --cpu 2 \
+    --num_samples 16 --perturbation_interval 3 --epochs 1800 \
+    --explore cifar10 --aug_policy cifar10 \
+    --lr 0.1 --wd 0.0005 --bs 8 --test_bs 8 --recompute_dset_stats
