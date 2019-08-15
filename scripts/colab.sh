9c71f1697a265994816b02643f68c962cbe32173
Ivan Pereira <navi1921@gmail.com>
2019-08-14 23:35:16 -0300

    Update colab.sh

--- scripts/colab.sh
+++ scripts/colab.sh
@@ -3,12 +3,12 @@ export PYTHONPATH="$(pwd)"
 
 python pba/search2.py \
     --local_dir "$PWD/results/" \
-    --model_name wrn_40_2 \
+    --model_name shake_shake_96 \
     --dataset test \
     --data_path /content/data/ \
-    --train_size 2000 --val_size 2322 \
-    --checkpoint_freq 5 \
-    --name "test_search" --gpu 0.15 --cpu 2 \
-    --num_samples 16 --perturbation_interval 3 --epochs 200 \
+    --train_size 2600 --val_size 2600 \
+    --checkpoint_freq 15 \
+    --name "test_search" --gpu 1 --cpu 2 \
+    --num_samples 16 --perturbation_interval 3 --epochs 1800 \
     --explore cifar10 --aug_policy cifar10 \
-    --lr 0.1 --wd 0.0005 --bs 8 --test_bs 8 --recompute_dset_stats
+    --lr 0.01 --wd 0.001 --bs 8 --test_bs 8 --recompute_dset_stats
