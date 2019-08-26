e09602ce47a4d07d52da4d10989de5154c5b607c
Ivan Pereira <navi1921@gmail.com>
2019-08-26 15:57:17 -0300

    Update trainSamantha.sh

--- scripts/trainSamantha.sh
+++ scripts/trainSamantha.sh
@@ -1,37 +1,15 @@
-#!/bin/bash
 export PYTHONPATH="$(pwd)"
+#--use_hp_policy --hp_policy /home/ivan/DeepAugmentColab/pbt_policy_4.txt \
 
-# args: [model name] [lr] [wd]
-eval_cifar10() {
-  hp_policy="/home/ivan/DeepAugmentColab/pbt_policy_4.txt"
-  local_dir="$PWD/results/"
-  data_path="~/geo/data/"
-
-  size=50000
-  dataset= 'test'
-  name="eval_cifar10_$1" # has 8 cutout size
-
-  python pba/train.py \
-    --local_dir "$local_dir" --data_path "$data_path" \
-    --model_name "$1" --dataset "$dataset" \
-    --train_size "$size" --val_size 1322 \
+python pba/train.py \
+    --local_dir ~/geo/pba/results/ --data_path ~/geo/data/ \
+    --model_name wrn_28_10 --dataset "test" \
+    --train_size 1000 --val_size 1322 \
     --checkpoint_freq 25 --gpu 1 --cpu 2 \
-    --use_hp_policy --hp_policy "$hp_policy" \
+    --use_hp_policy --hp_policy /home/ivan/geo/pba/schedules/rcifar10_16_wrn.txt \
     --hp_policy_epochs 200 \
-    --aug_policy cifar10 --name "$name" \
-    --lr "$2" --wd "$3"
-}
-
-if [ "$@" = "wrn_28_10" ]; then
-  eval_cifar10 wrn_28_10 0.1 0.0005
-elif [ "$@" = "ss_32" ]; then
-  eval_cifar10 shake_shake_32 0.01 0.001
-elif [ "$@" = "ss_96" ]; then
-  eval_cifar10 shake_shake_96 0.01 0.001
-elif [ "$@" = "ss_112" ]; then
-  eval_cifar10 shake_shake_112 0.01 0.001
-elif [ "$@" = "pyramid_net" ]; then
-  eval_cifar10 pyramid_net 0.05 0.00005
-else
-  echo "invalid args"
-fi
+    --aug_policy cifar10 --name "test_search" \
+    --lr 0.1 --wd 0.0005 \
+    --recompute_dset_stats
+~                                                                               
+~                                   
