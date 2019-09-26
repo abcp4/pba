0c1fd95c5bf9bfcfd0ec1acf782499a097192e3e
Ivan Pereira <navi1921@gmail.com>
2019-09-26 03:27:58 -0300

    Update data_utils.py

--- pba/data_utils.py
+++ pba/data_utils.py
@@ -72,7 +72,7 @@ def load(ttype,label,l,cv_img,labels):
         cv_img.append(n)
         labels.append(l)
         names.append(img)
-     return cv_img,labels,names
+    return cv_img,labels,names
 
 class DataSet(object):
     """Dataset object that produces augmented training and eval data."""
