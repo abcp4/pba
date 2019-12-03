9629c0ca222d859c15a05d5af2024b0bff9489da
Ivan Pereira <navi1921@gmail.com>
2019-12-02 22:04:54 -0300

    Update trainColab.sh

--- scripts/trainColab.sh
+++ scripts/trainColab.sh
@@ -1,6 +1,5 @@
 export PYTHONPATH="$(pwd)"
 #--use_hp_policy --hp_policy /home/ivan/DeepAugmentColab/pbt_policy_4.txt \
-
 python pba/train.py \
     --local_dir /content/pba/results/ --data_path /content/data/ \
     --model_name wrn_40_2 \
@@ -11,5 +10,5 @@ python pba/train.py \
     --use_hp_policy --hp_policy /content/pba/schedules/aug.txt \
     --hp_policy_epochs 200 \
     --aug_policy cifar10 --name "test_search" \
-    --lr 0.1 --wd 0.0005 \
+    --lr 0.01 --wd 0.0005 \
     --recompute_dset_stats
