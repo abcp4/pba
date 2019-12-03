b3e28a5141b7284cc7fe479ae04ac3294a0b6da7
Ivan Pereira <navi1921@gmail.com>
2019-12-02 21:26:45 -0300

    Update trainColab.sh

--- scripts/trainColab.sh
+++ scripts/trainColab.sh
@@ -3,10 +3,10 @@ export PYTHONPATH="$(pwd)"
 
 python pba/train.py \
     --local_dir /content/pba/results/ --data_path /content/data/ \
-    --model_name wrn_28_10 --dataset "test" \
-    --train_size 1000 --val_size 1322 \
+    --model_name wrn_40_2 --dataset test \
+    --train_size 7000 --val_size 1322 \
     --checkpoint_freq 25 --gpu 1 --cpu 2 \
-    --use_hp_policy --hp_policy /content/pba/schedules/rcifar10_16_wrn.txt \
+    --use_hp_policy --hp_policy /content/pba/schedules/aug.txt \
     --hp_policy_epochs 200 \
     --aug_policy cifar10 --name "test_search" \
     --lr 0.1 --wd 0.0005 \
